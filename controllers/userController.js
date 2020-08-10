0a7f36410eb7123e73545ee130fa8ffe5d94a853
choihanna <choihanna@DESKTOP-P95JTFQ>
2020-08-10 21:11:38 +0900

    Change Profile HTML

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ export const changePassword = (req, res) => res.render("changePassword");
 
 /*
 res.send말고 HTML 페이지를 넘기려면 res.render or res.redirect를 써야함
-그리고 저기 안에 들어가는 택스트 이름이 pug 파일 이름과 같아야해 그래서 render 함수 자체가 views폴더로 가서 
+인자안에 텍스트 이름이 pug 파일 이름과 같아야함 그래야 render 함수 자체가 views폴더로 가서 
 알아서 함수 인자로 들어온 텍스트와 views 폴더에 있는 파일을 매칭해서 그 HTML을 뿌려줌
 */
 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,20 @@
 import express from "express";
 import routes from "../routes";
-import { editProfile } from "../controllers/userController";
+import {  //page에 해당
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.editProfile, editProfile); //userDetail보다 위에 있어야함 -> id가 editProfile로 인식
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter; 
+
+/* 라우트를 만들지 않으면 페이지로 이동이 안됨 get method인 이유는 editProfile의 화면을 보려면 
+일단 get으로 받아와야함 post method는 editProfile.pug의 내용들을 전달해줘야되기 때문 
+*/
