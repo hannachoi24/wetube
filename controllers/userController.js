b3b88dd493f766259b773b6786daccd68db43b3d
choihanna <choihanna@DESKTOP-P95JTFQ>
2020-08-11 20:34:24 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,29 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join "});
+};
+
+export const postJoin = (req, res) => { //method가 post인(join.pug) 경로에서만 작동
+  const {
+    body: { name, email, password, password2 }
+  } = req; 
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join "}); //post로 이동해서 join화면을 render
+  } else {
+    //To Do: Register User
+    //To Do: Log user in
+    res.redirect(routes.home); //위에다 import routes를 해줘야함(외부디렉토리형식으로)
+  }   
+};
+// join하면 login창으로가고 그다음 home화면으로 갈 수 있게 하려고 -> globalRouter.js에서도 이름 변경
+
+export const login = (req, res) => res.render("login", { pageTitle: "Log In"} );
+export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out"});
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail"});
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password"});
 
 /*
 res.send말고 HTML 페이지를 넘기려면 res.render or res.redirect를 써야함
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,21 @@
 import express from "express";
 import routes from "../routes"; // 이 디렉토리 바깥에서 실행되야되기 때문에 ../
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import { 
+    getJoin, 
+    login, 
+    logout,
+    postJoin 
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+
 globalRouter.get(routes.home, home); // home은 controllers/videoController에서 home을 auto import함 -> export 해줬을 때만 작동
 globalRouter.get(routes.search, search); // search도 마찬가지
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
