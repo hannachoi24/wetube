45ac74815d7ad9bcd7c17aa12caaab5c172b059e
choihanna <hnc24@naver.com>
2020-08-12 23:39:33 +0900

    Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,17 @@
 import routes from "../routes"; //default export할 때는 이런형식으로 import
+import Video from "../models/Video";
 
-export const home = (req, res) => {
+export const home = async (req, res) => { //async는 JS가 어떤부분을 기다려주게함(JS는 한번에 많은일을해서 기다려주지않기때문에)
+  try{
+  const videos = await Video.find({}); //Database에 있는 모든 Video를 가져옴 
   res.render("home", { pageTitle: "Home", videos }); //render(템플릿, 템플릿에 추가할 정보가 담긴 객체) 함수의 인자로 템플릿 파일의 이름을 입력하면 됨(views폴더에 파일명이home이고 확장자가 pug인 것을 찾은 후 보여줌)
-};                                                //home 템플릿에 videos가 전달됨
-                                                                               
+                                                     //home 템플릿에 videos가 전달됨
+  } catch(error) {                                          
+  console.log(error);
+  res.render("home", { pageTitle: "Home", videos: [] });
+  }                                                                                                    
+};           
+                                                                    
 export const search = (req, res) => {
   const {query: { term: searchingBy }
 } = req; //= const searchingBy = req,query.term 
