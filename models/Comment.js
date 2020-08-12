fc0798f4cd1969733cf366b7b352096bbb07b618
choihanna <hnc24@naver.com>
2020-08-12 22:50:41 +0900

    Comment Model

---
+++ models/Comment.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: "Text is required"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now // 현재 날짜를 반환하는 function
+    },
+    /* video와 comment를 연결하는방법 #1: comment에 연결된 videoID를 줄것인가
+    video: { 
+        type: mongoose.Schema.Types.ObjectId, // 이 comment는 video와 연결되어 있음
+        ref: "Video" // 어떤 objectID가 어느 model에서 온건지를 알려줌
+    }
+    */
+});
+
+const model = mongoose.model("Comment", CommentSchema);
+export default model;
--- models/Video.js
+++ models/Video.js
@@ -12,7 +12,8 @@ const VideoSchema = new mongoose.Schema({
         type: String,
         required: "Title is required"
     },
-    description: String,
+    description: String, 
+// 무언가 줄 option이 있다면 object로 만들어야하고(configure이 필요) 그렇지 않으면 한 줄로 작성
     views: {
         type: Number,
         default: 0 // 처음 video가 생성되면 views를 0이 되게함
@@ -20,7 +21,14 @@ const VideoSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now // 현재 날짜를 반환하는 function
-    }
+    },
+    // video와 comment를 연결하는방법 #2: 모든 commentID들을 array로 video에 집어 넣을것인가
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId, //이 comment는 video와 연결되어 있음
+            ref: "Comment" // 어떤 objectID가 어느 model에서 온건지를 알려줌
+        }
+    ]
 });
 // 이제 이 스키마(definition)를 이용하여 model(document)을 만들어준다.
 // 모델의 이름은 "Video", Video 모델의 스키마는 VideoSchema
