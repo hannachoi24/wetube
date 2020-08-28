4d117207e6025a6666996d8ab97683167363a90c
choihanna <hnc24@naver.com>
2020-08-28 18:46:40 +0900

    Recording Video part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,24 +2,41 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = (event) => {
+  // stream에서 가진 video를 recording 하는 것
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      // media를 user로 부터 얻어와 비디오에 넣어줌->비디오도 얻어야함
+      // media를 user로 부터 얻어와 비디오에 넣어줌(비디오도 얻어야함을 의미)
       audio: true,
       video: { width: 1280, height: 720 },
     });
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "☹️ Cant record";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
